130f0ce94be0c1ba3939b87b6c68558fe6fbe415
Ledesma <alexander.ledesma@accenturefederal.com>
2020-10-08 16:02:01 -0400

    Dont start a snapshot if there is already one pending

--- snappy/snappy.py
+++ snappy/snappy.py
@@ -18,6 +18,11 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Snappy manages snapshots"""
@@ -105,6 +110,9 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print(" Skipping {0}, snapshot already in progress".format(v.id))
+                continue
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by automated python script")
         print("Starting {0}....".format(i.id))
