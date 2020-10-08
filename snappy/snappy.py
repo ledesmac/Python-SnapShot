27585035588f601a6a5ec4b3f0f97091a3992b35
Ledesma <alexander.ledesma@accenturefederal.com>
2020-10-08 15:56:47 -0400

    Show all snapshots in list if --all is given

--- snappy/snappy.py
+++ snappy/snappy.py
@@ -28,7 +28,9 @@ def snapshots():
 @snapshots.command('list')
 @click.option('--project', default=None,
     help='Only snapshots for project (tag Project:<name>)')
-def list_snapshot(project):
+@click.option('--all', 'list_all', default=False, is_flag=True, 
+    help= "List all snapshots for each volume, not just most recent")
+def list_snapshots(project, list_all):
 
     instances = filter_instances(project)
 
@@ -43,6 +45,8 @@ def list_snapshot(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if s.state == "completed" and not list_all: break
 
     return
 
